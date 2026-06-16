89ddc42718a689fc9b8bf7879edce943f24b0ec1
Aidan-R-1032 <agruiz6742@gmail.com>
2026-06-16 11:22:11 -0500

    Created the TaskItem model

---
+++ Models/TaskItem.cs
@@ -0,0 +1,19 @@
+﻿namespace TaskTracker.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }                                     // Unique identifier/primary key for the task
+        public string Title { get; set; } = string .Empty;              // Title of the task, initialized to an empty string
+        public string Description { get; set; } = string.Empty;         // Detailed description of the task, initialized to an empty string
+        public DateTime DueDate { get; set; }                           // Due date for the task
+        public bool isIsCompleted { get; set; }                         // Indicates whether the task is completed or not
+        public Priority Priority { get; set; } = Priority.Medium;       // Priority level of the task, initialized to Medium by default
+        public DateTime CreatedAt { get; set; } = DateTime.Now;         // Timestamp for when the task was created, initialized to the current date and time
+    }
+    public enum Priority
+    {
+        Low,
+        Medium,
+        High
+    }
+}
