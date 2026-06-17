2d2515341128f450efd82928be2f0ec547b77114
Aidan-R-1032 <agruiz6742@gmail.com>
2026-06-17 14:06:38 -0500

    forgot to save TaskMapper before initial push

--- Dtos/TaskMapper.cs
+++ Dtos/TaskMapper.cs
@@ -1,6 +1,26 @@
-﻿namespace TaskTracker.Dtos
+﻿using TaskTracker.Models;
+
+namespace TaskTracker.Dtos
 {
     public class TaskMapper
     {
+        public static TaskResponseDto ToResponseDto(TaskItem task)
+        {
+            return new TaskResponseDto
+            {
+                Id = task.Id,
+                Title = task.Title,
+                Description = task.Description,
+                DueDate = task.DueDate,
+                IsCompleted = task.IsCompleted,
+                Priority = task.Priority,
+                CreatedAt = task.CreatedAt
+            };
+        }
+
+        public static IEnumerable<TaskResponseDto> ToResponseDtoList(IEnumerable<TaskItem> tasks)
+        {
+            return tasks.Select(t => ToResponseDto(t)).ToList();
+        }
     }
 }
