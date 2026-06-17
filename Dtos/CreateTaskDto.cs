302ce5923dedf2ab469090a4048c15dbac629988
Aidan-R-1032 <agruiz6742@gmail.com>
2026-06-17 14:04:56 -0500

    Created DTOs and TaskMapper to enable layer decoupling

---
+++ Dtos/CreateTaskDto.cs
@@ -0,0 +1,13 @@
+﻿using TaskTracker.Models;
+
+namespace TaskTracker.Dtos
+{
+    public class CreateTaskDto
+    // what comes IN - controls what the API accepts from the client
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime DueDate { get; set; }
+        public Priority Priority { get; set; } = Priority.Medium;
+    }
+}
---
+++ Dtos/TaskResponseDto.cs
@@ -0,0 +1,17 @@
+﻿using TaskTracker.Models;
+
+namespace TaskTracker.Dtos
+{
+    public class TaskResponseDto
+    // What goes OUT - controls what the API exposes to the client
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+        public Priority Priority { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsOverdue => !IsCompleted && DueDate < DateTime.UtcNow; // No DB query is needed
+    }
+}
